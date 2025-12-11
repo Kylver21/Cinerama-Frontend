ddd8c522598065f2a85b64e6569e8a4529424b96
Kylver Paucar <kilverpaucar1@gmail.com>
2025-12-11 18:52:24 -0500

    Configura apiUrl de producción (Render)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api',
+  apiUrl: 'https://cinerama-backend-1.onrender.com/api',
   tmdbApiKey: 'your-production-tmdb-api-key',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p',
